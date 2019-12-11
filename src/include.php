ceeecff2fe445e83fa3eda8d8d51ad286b866b5f
Matthew LeFevre <matthewlefevre95@gmail.com>
2019-12-11 07:48:12 -0700

    Starting to implement psr-4 autoloading.

--- src/include.php
+++ src/include.php
@@ -15,16 +15,16 @@ $dotenv->load();
  *  created.
  */
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/dbConnect.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/response.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/jwt.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/ken.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/route.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/controller.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/dispatcher.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/dbConnect.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/response.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/jwt.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/ken.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/route.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/controller.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/utilities/dispatcher.php';
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/core/test/controller.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/core/account/controller.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/core/account/endpoints.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/core/account/model.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/src/core/account/seed.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/core/test/controller.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/core/account/controller.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/core/account/endpoints.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/core/account/model.php';
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/src/core/account/seed.php';
